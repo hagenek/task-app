cb4a3fae062b59496c461d26f3af3bf2a61cdcac
Georg Ekeberg <georghagen@gmail.com>
2020-08-03 14:24:38 +0200

    added async test

--- src/math.js
+++ src/math.js
@@ -10,8 +10,21 @@ const celsisuToFahrenheit = (temp) => {
   return (temp * 1.8) + 32;
 };
 
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          if (a < 0 || b < 0) {
+              return reject('Numbers must be non-negative')
+          }
+
+          resolve(a + b)
+      }, 200)
+  })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
     celsisuToFahrenheit,
+    add,
   }
--- test/math.test.js
+++ test/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsisuToFahrenheit } = require("../src/math")
+const { calculateTip, fahrenheitToCelsius, celsisuToFahrenheit, add } = require("../src/math")
 
 test("Should calculate total with tip", () => {
   const total = calculateTip(10, 0.3);
@@ -19,3 +19,25 @@ test("Should calculate total with default tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
 })
+
+test("Async test demo",  (done) => {
+
+  setTimeout(() => {
+    expect(2).toBe(2); 
+    done(); 
+  }, 2000)
+})
+
+test("Should add two numbers", (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  })
+})
+
+test("Should add two numbers async/await", async () => {
+  const sum = await add(10,22);
+  expect(sum).toBe(32);
+})
+
+
